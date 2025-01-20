c20da0992b67176458cf27c84e67baba144bdc93
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-01-21 02:31:08 +0530

    updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,15 @@
 import DownArrow from "../assets/icons/downarrow.svg";
 import { notFoundPageDetails } from "../libraries/constants";
 import CustomNavigationButton from "../utilities/CustomNavigationButton";
+import { themeContext } from "../contexts/themeContext";
+import { useContext } from "react";
 
 const NotFound = () => {
   document.title = notFoundPageDetails.title;
   document
     .querySelector('meta[name="description"]')
     .setAttribute("content", notFoundPageDetails.description);
+  const { theme } = useContext(themeContext);
   return (
     <div className="mx-auto my-32 flex w-[70%] flex-col items-center justify-center gap-10 text-center">
       <span className="text-2xl xl:text-6xl 2xl:text-8xl">
@@ -19,10 +22,10 @@ const NotFound = () => {
         <img
           src={DownArrow}
           alt="Down Arrow Icon"
-          className="h-12 w-12 xl:h-24 xl:w-24"
+          className={`h-12 w-12 xl:h-24 xl:w-24 ${theme === "dark" ? "" : "invert"}`}
         />
       </span>
-      <CustomNavigationButton name="Home" path="/" />
+      <CustomNavigationButton name="Back to Home Page" path="/" />
     </div>
   );
 };
